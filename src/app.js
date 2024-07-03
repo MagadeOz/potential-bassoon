86419d70b1e10bc70955559052969c661c5cbf62
Miranda <77069174+MagadeOz@users.noreply.github.com>
2024-07-03 16:32:00 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♠", "♣", "♥", "♦"];
+  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+
+  function random(arr) {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+  }
+
+  let paloRandom = random(palos);
+  let numeroRandom = random(numeros);
+
+  let htmlNumber = document.querySelector("#card-number");
+  let htmlPaloTop = document.querySelector("#card-palo-top");
+  let htmlPaloBottom = document.querySelector("#card-palo-bottom");
+
+  htmlNumber.innerHTML = numeroRandom;
+  htmlPaloTop.innerHTML = paloRandom;
+  htmlPaloBottom.innerHTML = paloRandom;
+
+  if (paloRandom === "♥" || paloRandom === "♦") {
+    htmlPaloTop.style.color = "red";
+    htmlPaloBottom.style.color = "red";
+  }
 };
